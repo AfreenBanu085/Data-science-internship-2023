08de912985e5902239ce8484c426cfa442dbdbfa
Afreen Banu <119496677+AfreenBanu085@users.noreply.github.com>
2023-02-27 13:14:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.header('Hello Data Science Internship feb 2023!')
+st.title('Data Science Internship feb 2023!')
+
+st. header(':green[Hello], I am Afreen banu:wave:')
+
+st.subheader('Lets connect!')
+
+
+if st.button('Linkdin'):
+    st.write('https://www.linkedin.com/in/afreen-banu-826130258/')
+else: 
+    st.write()    
+
+if st.button('Github'):
+    st.write('https://github.com/AfreenBanu085')
+else: 
+    st.write()    
